2c513554c9e9bab067c0759a8580c3559cf24a8d
Pranpaveen Laykaviriyakul <pranpaveen.lay@gmail.com>
2021-02-10 13:28:35 +0700

    add-disco-model

--- src/layyer/discodef/utils.py
+++ src/layyer/discodef/utils.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from getData import *
+from .getData import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
