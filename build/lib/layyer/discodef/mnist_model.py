db48b5c38098ee93d2d34693d98e5ef4d319d919
ปรานต์ปวีณ เลขวิริยะกุล <pranpaveen.lay@gmail.com>
2021-08-20 14:25:07 +0700

    update

--- build/lib/layyer/discodef/mnist_model.py
+++ build/lib/layyer/discodef/mnist_model.py
@@ -25,7 +25,48 @@ class mnistmodel_A(nn.Module):
         return x
 
 
+
 class mnistmodel_B(nn.Module):
+    def __init__(self):
+        super(mnistmodel_B, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=8, stride=2, padding=3)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=6, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=5, stride=1)
+        self.dense1 = nn.Linear(in_features=128, out_features=128)
+        self.dense2 = nn.Linear(in_features=128, out_features=10)
+
+    def forward(self, x):
+        x = F.dropout(x, 0.2)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, 128)
+        x = F.relu(self.dense1(x))
+        x = F.dropout(x, 0.5)
+        x = self.dense2(x)
+        return x
+
+
+class mnistmodel_C(nn.Module):
+    def __init__(self):
+        super(mnistmodel_C, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=5, stride=2)
+        self.dense1 = nn.Linear(in_features=12 * 12 * 64, out_features=128)
+        self.dense2 = nn.Linear(in_features=128, out_features=10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, 12 * 12 * 64)
+        x = F.dropout(x, 0.25)
+        x = F.relu(self.dense1(x))
+        x = F.dropout(x, 0.5)
+        x = self.dense2(x)
+        return x
+
+
+class mnistmodel_B_old(nn.Module):
     def __init__(self):
         super(mnistmodel_B, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=8, stride=2, padding=3)
@@ -47,7 +88,7 @@ class mnistmodel_B(nn.Module):
         return x
 
 
-class mnistmodel_C(nn.Module):
+class mnistmodel_C_old(nn.Module):
     def __init__(self):
         super(mnistmodel_C, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=128, kernel_size=3, padding=1)
